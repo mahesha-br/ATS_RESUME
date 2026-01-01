4532c17cf3dcec8ddce4ddfc0ee0a3df92dd2acb
mahesha-br <maheshabr2004@gmail.com>
2026-01-01 20:06:42 +0530

    fourth update

--- src/component/Dashboard/Dashboard.jsx
+++ src/component/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 const Dashboard = () => {
   return (
-    <div className="flex-[0.8] h-screen overflow-auto p-[50px] flex justify-between bg-[whitesmoke] box-border">
-      <div className="w-[70%] p-[30px] h-full h-fit box-border bg-[#f4f6fa] rounded-[20px] shadow-[0_10px_20px_rgba(0,0,0,0.2)]"
+    <div className="flex-[0.8] h-screen overflow-auto p-12.5 flex justify-between bg-[whitesmoke] box-border">
+      <div className="w-[70%] p-7.5 h-full box-border bg-[#f4f6fa] rounded-[20px] shadow-[0_10px_20px_rgba(0,0,0,0.2)]"
 >
     <div className="text-[26px] ">
         Smart Resume Screening
@@ -10,7 +10,7 @@ const Dashboard = () => {
         Resume Match Score
     </div>
 
-    <div className="w-full box-border rounded-[30px] px-[20px] py-[15px] shadow-[0_4px_6px_rgba(0,0,0,0.1)] my-[40px] text-[16px] bg-white">
+    <div className="w-full box-border rounded-[30px] px-5 py-[15px] shadow-[0_4px_6px_rgba(0,0,0,0.1)] my-[40px] text-[16px] bg-white">
         <div className="flex items-center gap-2">
   <span>🔔</span>
   <span>Important Instruction:</span>
